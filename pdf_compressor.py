621fc788a79b974ba3a32105f51ef5f68ec2b796
Theo van der Sluijs <theo@vandersluijs.nl>
2019-01-13 10:11:33 +0100

    new version with class

--- pdf_compressor.py
+++ pdf_compressor.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
-# Author: Sylvain Carlioz
-# 6/03/2017
-# MIT license -- free to use as you want, cheers.
 
 """
+author: Pure Python
+url: https://www.purepython.org
+copyright: CC BY-NC 4.0
+Forked date: 2018-01-07 / First version MIT license -- free to use as you want, cheers.
+
+Original Author: Sylvain Carlioz, 6/03/2017
+
 Simple python wrapper script to use ghoscript function to compress PDF files.
+With this class you can compress and or fix a folder with (corrupt) PDF files.
+
+You can also use this class within your own scripts just do a
+import CompressPDF
 
 Compression levels:
     0: default
@@ -16,80 +24,97 @@ Compression levels:
 Dependency: Ghostscript.
 On MacOSX install via command line `brew install ghostscript`.
 """
-
-import argparse
 import subprocess
 import os.path
 import sys
-from shutil import copyfile
+import argparse
 
 
-def compress(input_file_path, output_file_path, power=0):
-    """Function to compress PDF via Ghostscript command line interface"""
-    quality = {
-        0: '/default',
-        1: '/prepress',
-        2: '/printer',
-        3: '/ebook',
-        4: '/screen'
-    }
+class CompressPDF:
+    def __init__(self, compress_level=0, show_info=False):
+        self.compress_level = compress_level
 
-    # Basic controls
-    # Check if valid path
-    if not os.path.isfile(input_file_path):
-        print("Error: invalid path for input PDF file")
-        sys.exit(1)
+        self.quality = {
+            0: '/default',
+            1: '/prepress',
+            2: '/printer',
+            3: '/ebook',
+            4: '/screen'
+        }
 
-    # Check if file is a PDF by extension
-    if input_file_path.split('.')[-1].lower() != 'pdf':
-        print("Error: input file is not a PDF")
-        sys.exit(1)
+        self.show_compress_info = show_info
 
-    print("Compress PDF...")
-    initial_size = os.path.getsize(input_file_path)
-    subprocess.call(['gs', '-sDEVICE=pdfwrite', '-dCompatibilityLevel=1.4',
-                    '-dPDFSETTINGS={}'.format(quality[power]),
-                    '-dNOPAUSE', '-dQUIET', '-dBATCH',
-                    '-sOutputFile={}'.format(output_file_path),
-                     input_file_path]
-    )
-    final_size = os.path.getsize(output_file_path)
-    ratio = 1 - (final_size / initial_size)
-    print("Compression by {0:.0%}.".format(ratio))
-    print("Final file size is {0:.1f}MB".format(final_size / 1000000))
-    print("Done.")
+    def compress(self, file=None, new_file=None):
+        """
+        Function to compress PDF via Ghostscript command line interface
+        :param file: old file that needs to be compressed
+        :param new_file: new file that is commpressed
+        :return: True or False, to do a cleanup when needed
+        """
+        try:
+            if not os.path.isfile(file):
+                print("Error: invalid path for input PDF file")
+                sys.exit(1)
+
+            # Check if file is a PDF by extension
+            filename, file_extension = os.path.splitext(file)
+            if file_extension != '.pdf':
+                raise Exception("Error: input file is not a PDF")
+                return False
+
+            if self.show_compress_info:
+                initial_size = os.path.getsize(file)
+
+            subprocess.call(['gs', '-sDEVICE=pdfwrite', '-dCompatibilityLevel=1.4',
+                            '-dPDFSETTINGS={}'.format(self.quality[self.compress_level]),
+                            '-dNOPAUSE', '-dQUIET', '-dBATCH',
+                            '-sOutputFile={}'.format(new_file),
+                             file]
+            )
 
 
-def main():
-    parser = argparse.ArgumentParser(
-        description=__doc__,
-        formatter_class=argparse.RawDescriptionHelpFormatter
-    )
-    parser.add_argument('input', help='Relative or absolute path of the input PDF file')
-    parser.add_argument('-o', '--out', help='Relative or absolute path of the output PDF file')
-    parser.add_argument('-c', '--compress', type=int, help='Compression level from 0 to 4')
-    parser.add_argument('--open', action='store_true', default=False,
-                        help='Open PDF after compression')
-    args = parser.parse_args()
+            if self.show_compress_info:
+                final_size = os.path.getsize(new_file)
+                ratio = 1 - (final_size / initial_size)
+                print("Compression by {0:.0%}.".format(ratio))
+                print("Final file size is {0:.1f}MB".format(final_size / 1000000))
 
-    # In case no compression level is specified, default is 2 '/ printer'
-    if not args.compress:
-        args.compress = 2
-    # In case no output file is specified, store in temp file
-    if not args.out:
-        args.out = 'temp.pdf'
+            return True
+        except Exception as error:
+            print('Caught this error: ' + repr(error))
+        except subprocess.CalledProcessError as e:
+            print("Unexpected error:".format(e.output))
+            return False
 
-    # Run
-    compress(args.input, args.out, power=args.compress)
-
-    # In case no output file is specified, erase original file
-    if args.out == 'temp.pdf':
-        copyfile(args.out, args.input)
-        os.remove(args.out)
-
-    # In case we want to open the file after compression
-    if args.open:
-        subprocess.call(['open', args.out])
 
 if __name__ == '__main__':
-    main()
+
+    parser = argparse.ArgumentParser(
+        description='''Welcome to this helpfile. ''',
+        epilog="""Thats all folks!""")
+    parser.add_argument('-sf', '--startFolder', help='Start Folder Domain', required=False, type=str)
+    parser.add_argument('-cl', '--compressLevel', type=int, help='Compression level from 0 to 4', default=2)
+    parser.add_argument('-s', '--showInfo', type=int, help='Show extra compression information 0 or 1', default=0)
+    args = parser.parse_args()
+
+    '''when where is no start folder full stop!'''
+    if args.startFolder is not None and args.startFolder != "":
+        start_folder = args.startFolder
+
+        p = CompressPDF(args.compressLevel)
+
+        compress_folder = os.path.join(start_folder, "compressed_folder/")
+        if not os.path.exists(compress_folder):
+            os.makedirs(compress_folder)
+
+        '''Loop within folder over PDF files'''
+        for filename in os.listdir(args.startFolder):
+            my_name, file_extension = os.path.splitext(filename)
+            if file_extension == '.pdf':
+                file = os.path.join(start_folder, filename)
+                new_file = os.path.join(compress_folder, filename)
+
+                if p.compress(file, new_file):
+                    print("{} done!".format(filename))
+                else:
+                    print("{} gave an error!".format(file))
